46bc18084ece077c244111329c7d8165ec8fadf4
Patrik Nygren <patriknygren82@gmail.com>
2023-04-28 15:02:16 +0200

    Navigation: part 11

--- Demos/4.Navigation/App.js
+++ Demos/4.Navigation/App.js
@@ -1,15 +1,25 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 // https://reactnavigation.org/ gå igenom lite om dokumentation
 // https://reactnavigation.org/docs/stack-navigator
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
