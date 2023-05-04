6ff8c954b7620731eac0e874470ccee0c6a51a72
Patrik Nygren <patriknygren82@gmail.com>
2023-05-04 13:57:37 +0200

    Navigation: part 19

--- Demos/4.Navigation/components/MealItem.js
+++ Demos/4.Navigation/components/MealItem.js
@@ -3,7 +3,10 @@ import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
     <View style={styles.mealItem}>
-      <Pressable>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
         <View>
           <Image source={{ uri: imageUrl }} style={styles.image} />
           <Text style={styles.title}>{title}</Text>
@@ -29,6 +32,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+  },
+  buttonPressed: {
+    opacity: 0.5,
   },
   image: {
     width: "100%",
