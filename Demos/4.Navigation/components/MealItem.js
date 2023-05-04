0e2c861587296217bbd971d9ac7c5f56746aba38
Patrik Nygren <patriknygren82@gmail.com>
2023-05-04 13:48:27 +0200

    Navigation: part 18

--- Demos/4.Navigation/components/MealItem.js
+++ Demos/4.Navigation/components/MealItem.js
@@ -2,7 +2,7 @@ import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
 const MealItem = ({ title, imageUrl }) => {
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
         <View>
           <Image source={{ uri: imageUrl }} style={styles.image} />
@@ -14,6 +14,17 @@ const MealItem = ({ title, imageUrl }) => {
 };
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
   image: {
     width: "100%",
     height: 200,
