11d1132a60fab9770cce0a89880e469469d85d91
Patrik Nygren <patriknygren82@gmail.com>
2023-05-04 12:38:30 +0200

    Navigation: part 16

--- Demos/4.Navigation/screens/MealsOverviewScreen.js
+++ Demos/4.Navigation/screens/MealsOverviewScreen.js
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem.js";
 
 const MealsOverviewScreen = () => {
   const route = useRoute();
   const categoryId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.includes(categoryId);
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen - {categoryId} </Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
