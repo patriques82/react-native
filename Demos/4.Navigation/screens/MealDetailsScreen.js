bc624a59750245140bad396f84dfad5799552672
Patrik Nygren <patriknygren82@gmail.com>
2023-05-05 15:46:54 +0200

    Navigation: part 28

--- Demos/4.Navigation/screens/MealDetailsScreen.js
+++ Demos/4.Navigation/screens/MealDetailsScreen.js
@@ -17,10 +17,45 @@ const MealDetailsScreen = () => {
   });
 
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: meal.imageUrl }} />
+      <Text style={styles.title}>{meal.title}</Text>
+
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Text>Ingredients</Text>
+
+          <Text>Steps</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
 
 export default MealDetailsScreen;
