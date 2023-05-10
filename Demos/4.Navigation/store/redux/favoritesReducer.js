b8b281ee70448b50604fb950c611fa9de06f8b39
Patrik Nygren <patriknygren82@gmail.com>
2023-05-10 11:52:35 +0200

    Statemng: part 13

---
+++ Demos/4.Navigation/store/redux/favoritesReducer.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Slice of state
+const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState: {
+    ids: [],
+  },
+  reducers: {
+    addFavorite: (state, action) => {
+      // toolkit låter oss mutera state
+      state.ids.push(action.payload.id);
+    },
+    removeFavorite: (state, action) => {
+      state.ids.splice(state.ids.indexOf(action.payload.id), 1);
+    },
+  },
+});
+
+export const addFavorite = favoritesSlice.actions.addFavorite;
+export const removeFavorite = favoritesSlice.actions.removeFavorite;
+
+export default favoritesSlice.reducer; // intern grej i toolkit som låter oss merga reducers i configureStore
--- Demos/4.Navigation/store/redux/store.js
+++ Demos/4.Navigation/store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import favoritesReducer from "./favoritesReducer";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
 });
