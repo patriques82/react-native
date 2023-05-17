190f1148b8667ebdce204cca03292f06433815a6
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 12:03:40 +0200

    Summary: part 13

--- Demos/5.Summering/components/Expenses/ExpensesOutput.js
+++ Demos/5.Summering/components/Expenses/ExpensesOutput.js
@@ -2,6 +2,39 @@ import { View, Text, FlatList } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-01-05"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-12-01"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-19"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-18"),
+  },
+];
+
 const ExpensesOutput = ({ expenses, periodText }) => {
   return (
     <View>
