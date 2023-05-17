3154ea68101a17015369a807e7547f04375c5526
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 15:36:54 +0200

    Summary: part 35

--- Demos/5.Summering/screens/ManageExpense.js
+++ Demos/5.Summering/screens/ManageExpense.js
@@ -3,6 +3,7 @@ import { useLayoutEffect } from "react";
 import { View, StyleSheet } from "react-native";
 
 import IconButton from "../components/UI/IconButton";
+import Button from "../components/UI/Button";
 import { GlobalStyles } from "../constants/styles";
 
 const ManageExpense = ({ route }) => {
@@ -16,12 +17,28 @@ const ManageExpense = ({ route }) => {
     });
   }, [navigation, expenseId]);
 
+  const cancelHandler = () => {
+    console.log("cancel");
+  };
+
   const deleteHandler = () => {
     console.log("delete");
   };
 
+  const confirmHandler = () => {
+    console.log("confirm");
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       <IconButton
         icon="trash"
         size={36}
@@ -37,6 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
 
