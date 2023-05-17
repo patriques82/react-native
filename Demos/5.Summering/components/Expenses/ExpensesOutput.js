ce531bd5df4ba89581f1bd6ec60c6078070723a4
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 12:49:20 +0200

    Summary: part 21

--- Demos/5.Summering/components/Expenses/ExpensesOutput.js
+++ Demos/5.Summering/components/Expenses/ExpensesOutput.js
@@ -34,6 +34,36 @@ const DUMMY_EXPENSES = [
     amount: 18.59,
     date: new Date("2023-02-18"),
   },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-01-05"),
+  },
+  {
+    id: "e8",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-12-01"),
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-19"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-18"),
+  },
 ];
 
 const ExpensesOutput = ({ expenses, periodText }) => {
@@ -48,7 +78,9 @@ const ExpensesOutput = ({ expenses, periodText }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
