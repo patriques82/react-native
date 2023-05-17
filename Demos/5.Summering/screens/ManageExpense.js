1ccb4d49b92c7c5ddb44a841e108b88d4e622dc6
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 15:18:34 +0200

    Summary: part 31

--- Demos/5.Summering/screens/ManageExpense.js
+++ Demos/5.Summering/screens/ManageExpense.js
@@ -1,8 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
 const ManageExpense = ({ route }) => {
+  const navigation = useNavigation();
   const expenseId = route.params?.expenseId;
-  console.log(expenseId);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: expenseId !== undefined ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, expenseId]);
+
   return <Text>ManageExpense Screen</Text>;
 };
 
