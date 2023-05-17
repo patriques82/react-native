806b5f0d22eed5fe0a4d91f8be7176ee2ad6f48b
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 14:21:00 +0200

    Summary: part 24

--- Demos/5.Summering/App.js
+++ Demos/5.Summering/App.js
@@ -8,6 +8,7 @@ import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
 import { NavigationContainer } from "@react-navigation/native";
 import { GlobalStyles } from "./constants/styles";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -20,6 +21,9 @@ const ExpensesOverview = () => {
         headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton icon="add" size={24} color={tintColor} />
+        ),
       }}
     >
       <BottomTabs.Screen
