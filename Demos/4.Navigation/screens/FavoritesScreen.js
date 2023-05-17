44e944290f70a73041030a0b371de5b618a5fb3c
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 10:37:32 +0200

    Summary: part 0

--- Demos/4.Navigation/screens/FavoritesScreen.js
+++ Demos/4.Navigation/screens/FavoritesScreen.js
@@ -1,15 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
-import { useContext } from "react";
+import { useSelector } from "react-redux";
 
 import MealsList from "../components/MealsList";
-import { FavoritesContext } from "../store/context/FavoritesContext";
 import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealsIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
