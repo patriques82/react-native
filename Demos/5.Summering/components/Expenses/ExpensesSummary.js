bb01cacd7c069d5c14b3d26e6da079a1830b9881
Patrik Nygren <patriknygren82@gmail.com>
2023-05-17 12:20:51 +0200

    Summary: part 15

--- Demos/5.Summering/components/Expenses/ExpensesSummary.js
+++ Demos/5.Summering/components/Expenses/ExpensesSummary.js
@@ -1,13 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 const ExpensesSummary = ({ expenses, periodText }) => {
   const sum = expenses.reduce((sum, expense) => sum + expense.amount, 0);
   return (
-    <View>
-      <Text>{periodText}</Text>
-      <Text>{sum.toFixed(2)} SEK</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodText}</Text>
+      <Text style={styles.sum}>{sum.toFixed(2)} SEK</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary500,
+  },
+});
 
 export default ExpensesSummary;
