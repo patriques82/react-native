49b6b4768136a15888754555761432cc2c651749
Patrik Nygren <patriknygren82@gmail.com>
2023-05-18 09:27:30 +0200

    Summary: part 41

--- Demos/5.Summering/store/expenses_ctx.js
+++ Demos/5.Summering/store/expenses_ctx.js
@@ -1,6 +1,69 @@
 import { createContext, useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-01-05"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-12-01"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-19"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-18"),
+  },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-12-19"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-01-05"),
+  },
+  {
+    id: "e8",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-12-01"),
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-19"),
+  },
+  {
+    id: "e10  ",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-18"),
+  },
+];
+
 export const ExpensesCtx = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
@@ -30,7 +93,7 @@ const expenseReducer = (state, action) => {
 };
 
 const ExpensesCtxProvider = ({ children }) => {
-  const [expenseState, dispatch] = useReducer(expenseReducer, []);
+  const [expenseState, dispatch] = useReducer(expenseReducer, DUMMY_EXPENSES);
 
   const addExpense = (expense) => {
     dispatch({ type: "ADD", payload: expense });
